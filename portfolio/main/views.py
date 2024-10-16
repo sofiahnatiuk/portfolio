77fdba29f5439c070da40c4eaafb8337b4dcdcea
Sofia Hnatiuk <sofia.hnatiuk46@gmail.com>
2024-10-16 21:52:28 +0300

    gitignore

--- portfolio/main/views.py
+++ portfolio/main/views.py
@@ -1,3 +1,19 @@
+import requests
 from django.shortcuts import render
+import os
 
-# Create your views here.
+GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+
+
+def get_github_activity(username):
+    url = f"https://api.github.com/users/{username}/events/public"
+    headers = {"Authorization": f"token {GITHUB_TOKEN}"}
+    response = requests.get(url, headers=headers)
+    return response.json() if response.status_code == 200 else []
+
+
+def portfolio(request):
+    username = "sofiahnatiuk"
+    activity = get_github_activity(username)
+    context = {'activity': activity}
+    return render(request, 'main/portfolio.html', context)
--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-@)u1u_^y)#99!muo9a4e6v0zd&a@qb8=8+oo(dn(&kui^xso)t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.48.44.158', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -117,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
